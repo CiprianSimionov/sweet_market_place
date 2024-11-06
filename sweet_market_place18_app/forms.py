f39c18bbf0f66682b0ab6d1ba8e4d1e112d7e2e4
ciprian simionov <simionov.ciprian@gmail.com>
2024-11-06 20:04:48 +0200

    class for custom forms

--- sweet_market_place18_app/forms.py
+++ sweet_market_place18_app/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 from .models import Product
 
-#in cadrul acestui fisier vom crea toate clasele necesare pentru formulare personalizate
+#create here all clases needed for custom(default) forms
 
 class ProductForm(forms.ModelForm):
-    class Meta: #aceasta clasa ne ajuta sa modelam formularul din upload_product
+    class Meta: #clasa care ajuta sa modelam formularul din upload_product
         model = Product #facem referire la formularul pentru produs
         fields = [ #fieldurile echivalent cu coloanele din baza de date
             "name", "description", "price", "ingredients", "weight", "available", "image"
